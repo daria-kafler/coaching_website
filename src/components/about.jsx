bec4bb5ad7d99211bbcac5b63056975781e0ac27
ataabatay <aliataabatay@gmail.com>
2024-05-08 18:01:32 +0100

    completed about me page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,9 +1,16 @@
+import Daria from '../assets/Daria-coffee.jpg';
+
 export default function About() {
   return (
     <>
-      <section className="about">
-        <img src='../assets/' alt="" />
-        <h1>hello</h1>
+      <section id="about">
+        <div className="content-wrapper">
+          <img src={Daria} alt="daria-photo" />
+          <p className="about-me">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus maxime dolorum voluptate! In odio esse
+            perspiciatis neque, animi minima expedita optio nulla minus ipsa rem a dignissimos error officia itaque?
+          </p>
+        </div>
       </section>
     </>
   );
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <>
-      <section className="hero">
+      <section id="hero">
         <div className="content-wrapper">
           <h1 className='hero-title'>Find Daria, find happiness.</h1>
           <p className='hero-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae atque, veritatis, fugit corporis optio doloremque enim minima debitis quos totam dolor esse! Omnis perspiciatis optio corrupti, quos sed laudantium fugiat.</p>
