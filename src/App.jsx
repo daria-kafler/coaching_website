da74b7d09e4f8abf1ffe512625f55df579a77f5b
ataabatay <aliataabatay@gmail.com>
2024-05-09 18:40:39 +0100

    started to work on the testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 // components
 import Navigation from './components/navigation.jsx';
-import Hero from './components/hero.jsx'
+import Hero from './components/hero.jsx';
 import About from './components/about.jsx';
 import Services from './components/services.jsx';
+import Testimonials from './components/testimonials.jsx';
 
 function App() {
-
   return (
     <>
       <Navigation />
       <Hero />
       <About />
       <Services />
+      <Testimonials />
     </>
   );
 }
---
+++ src/components/testimonials.jsx
@@ -0,0 +1,11 @@
+export default function Testimonials() {
+  return (
+    <>
+      <section className="testimonials">
+        <div className="content-wrapper">
+          <h1>Hear from our success stories...</h1>
+        </div>
+      </section>
+    </>
+  );
+}
