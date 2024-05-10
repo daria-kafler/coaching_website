930d2e42d0db3188ba59e529574480cd15cabe67
ataabatay <aliataabatay@gmail.com>
2024-05-10 17:36:32 +0100

    added team up with me section, did some responsiveness work and design consistency tweaks

--- src/components/teamup.jsx
+++ src/components/teamup.jsx
@@ -1,4 +1,4 @@
-import Daria from '../assets/Daria-hat.jpg'
+import Daria from '../assets/Daria-hat.jpg';
 
 export default function Teamup() {
   return (
@@ -6,33 +6,30 @@ export default function Teamup() {
       <section id="team-up">
         <img src={Daria} alt="" />
         <div className="content-wrapper">
-
-          <div className="text-content">
-            <h1>Then team up with me:</h1>
-            <ul>
-              <li>
-                We'll delve into your movement habits, connection with food, and work productivity patterns. This will
-                allow us to focus on what truly matters to you and cut through the noise.
-              </li>
-              <li>
-                You'll receive a custom plan aligned with your health goals, covering nutrition, fitness, and practical
-                strategies tailored to your needs. We’ll find practical solutions that work for **you**, to help navigate
-                any overwhelm and keep you on track.
-              </li>
-              <li>
-                You’ll also receive guidance and strategies to enhance productivity in your work life. Whether it's
-                managing energy levels, prioritising tasks, or improving time management,
-              </li>
-              <li>
-                Count on me for real-time responses via WhatsApp, Signal or SMS Monday to Friday from 10am to 8pm to
-                tackle any challenges and sprinkle some encouragement throughout your day.
-              </li>
-              <li>
-                Every week, we'll catch up to chat about how you're doing and fine-tune our strategies to fit seamlessly
-                into your life.
-              </li>
-            </ul>
-          </div>
+          <h1>Team up with me</h1>
+          <ul>
+            <li>
+              We'll delve into your movement habits, connection with food, and work productivity patterns. This will
+              allow us to focus on what truly matters to you and cut through the noise.
+            </li>
+            <li>
+              You'll receive a custom plan aligned with your health goals, covering nutrition, fitness, and practical
+              strategies tailored to your needs. We’ll find practical solutions that work for **you**, to help navigate
+              any overwhelm and keep you on track.
+            </li>
+            <li>
+              You’ll also receive guidance and strategies to enhance productivity in your work life. Whether it's
+              managing energy levels, prioritising tasks, or improving time management,
+            </li>
+            <li>
+              Count on me for real-time responses via WhatsApp, Signal or SMS Monday to Friday from 10am to 8pm to
+              tackle any challenges and sprinkle some encouragement throughout your day.
+            </li>
+            <li>
+              Every week, we'll catch up to chat about how you're doing and fine-tune our strategies to fit seamlessly
+              into your life.
+            </li>
+          </ul>
         </div>
       </section>
     </>
